438413887b4d172233bf96328d16aca32d4575a9
Andy Brenneke <sneakymax@gmail.com>
2016-03-15 21:21:47 -0700

    Bugs can now be saved to the back end, with validation.

--- public/js/bug_tracker/views/bug_item.js
+++ public/js/bug_tracker/views/bug_item.js
@@ -11,11 +11,14 @@ define([
       'click .js_bug_edit': 'editBug',
       'click .js_bug_save': 'saveBug',
       'click .bug_remove': 'removeBug',
+
+      'input .summary-input': 'updateSummary', // I don't really like this, but don't know of a better way of doing it in marionette?
+      'input .description-input': 'updateDescription'
     },
 
     templateHelpers: function () {
       return {
-        error: this.model.validationError,
+        errors: this.model.validationError,
       };
     },
 
@@ -23,9 +26,51 @@ define([
       if (!this.model) {
         throw new Error('BugItemView requires a model');
       }
-      this.listenTo(this.model, 'change', this.render);
+
+      this.summary = this.model.get("summary");
+      this.description = this.model.get("description");
+
+      this.listenTo(this.model, 'change', this.modelChanged);
       this.listenTo(this.model, 'invalid', this.showError);
     },
+
+    modelChanged: function() {
+      this.summary = this.model.get("summary");
+      this.description = this.model.get("description");
+
+      this.render();
+    },
+
+    editBug: function() {},
+
+    saveBug: function() {
+      this.model.set({
+        summary: this.summary,
+        description: this.description
+      });
+
+      if(this.model.isValid()) {
+        this.model.set('editing', false);
+        this.model.save();
+      }
+    },
+
+    removeBug: function() {},
+
+    showError: function() {
+      this.render();
+    },
+
+    // Store these in temporary variables, which are then set in the model in saveBug. 
+    // If the bug is saved in these, then the view get rerendered and it's horrible.
+    // There might be a better way to do this since I'm unfamiliar with backbone and marionette
+    updateSummary: function(event) {
+      this.summary = event.target.value;
+    },
+
+    updateDescription: function(event) {
+      this.description = event.target.value;
+    }
 
     // @TODO implement functions to handle 'editBug'
     // @TODO implement functions to handle 'saveBug'
--- tests/unit/bug_tracker/views/bug_item.spec.js
+++ tests/unit/bug_tracker/views/bug_item.spec.js
@@ -1,14 +1,19 @@
 define([
   'backbone',
-  'bug_tracker/views/bug_item'
-], function (Backbone, BugItemView) {
+  'bug_tracker/views/bug_item',
+  'bug_tracker/models/bug'
+], function (Backbone, BugItemView, BugModel) {
   'use strict';
 
   describe('BugItemView', function () {
-    var view;
+    var view, model;
+
     beforeEach(function () {
+      model = new BugModel();
+      model.save = function() {}; // No server when testing
+  
       view = new BugItemView({
-        model: new Backbone.Model(),
+        model: model,
       });
     });
 
@@ -16,7 +21,39 @@ define([
       expect(view).toBeDefined();
     });
 
-    // @TODO write tests to match the implementation :)
+    it('should have empty properties to start with', function() {
+      expect(view.model.get('summary')).toBe('');
+    });
 
+    it('should not update the model until saveBug is called', function() {
+      view.updateSummary({ target: { value: 'summary here' } });
+
+      expect(view.model.get('summary')).toBe('');
+    });
+
+    it('should update the model with the view values when saveBug is called', function() {
+      view.updateSummary({ target: { value: 'summary here' } });
+      view.updateDescription({ target: { value: 'description here' } });
+
+      view.saveBug();
+
+      expect(view.model.get('summary')).toBe('summary here');
+      expect(view.model.get('description')).toBe('description here');
+    });
+
+    it('should stop editing when the bug is saved', function() {
+      view.updateSummary({ target: { value: 'summary here' } });
+      view.updateDescription({ target: { value: 'description here' } });
+
+      view.saveBug();
+
+      expect(view.model.get('editing')).toBe(false);
+    });
+
+    it('should not stop editing if there is a validation error', function() {
+      view.saveBug();
+
+      expect(view.model.get('editing')).toBe(true);
+    });
   });
 });
